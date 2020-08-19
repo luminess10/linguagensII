bef772e7072d59201ce7ea861af3553e90c72f47
lumi <karinalumi9@gmail.com>
2020-08-19 19:37:47 -0300

    Aula Angular

---
+++ atividade/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Livro } from '../cliente.model';
+
+@Component({
+  selector: 'app-cliente-lista',
+  templateUrl: './cliente-lista.component.html',
+  styleUrls: ['./cliente-lista.component.css']
+})
+export class ClienteListaComponent {
+  @Input() livros: Livro[] =[];
+
+}
